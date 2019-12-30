f973c890417a328a08d8849889dee34a719b71d6
huangqijun <Admin_117>
2019-12-30 20:42:23 +0800

    config

--- apollo-configservice/src/main/java/com/ctrip/framework/apollo/configservice/ConfigServiceApplication.java
+++ apollo-configservice/src/main/java/com/ctrip/framework/apollo/configservice/ConfigServiceApplication.java
@@ -3,10 +3,9 @@ package com.ctrip.framework.apollo.configservice;
 import com.ctrip.framework.apollo.biz.ApolloBizConfig;
 import com.ctrip.framework.apollo.common.ApolloCommonConfig;
 import com.ctrip.framework.apollo.metaservice.ApolloMetaServiceConfig;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
@@ -23,7 +22,8 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableAutoConfiguration // (exclude = EurekaClientConfigBean.class)
 @Configuration
 @EnableTransactionManagement
-@PropertySource(value = {"classpath:configservice.properties"})
+@EnableEurekaClient
+@PropertySource(value = {"classpath:application.yml"})
 @ComponentScan(basePackageClasses = {ApolloCommonConfig.class,
     ApolloBizConfig.class,
     ConfigServiceApplication.class,
